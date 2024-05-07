82c6b07f91139f0cfeae834be05cadbe73351992
Pedro V.R. <ptreyes2@gmail.com>
2024-05-07 13:30:41 -0400

    Added constructor which works with values that are not always 'filled out'

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 require ('pet.php');
 
 // Instantiate a pet object
-$pet1 = new Pet();
+$pet1 = new Pet('Clifford', 'Red');
+$pet2 = new Pet('Bob');
+$pet3 = new Pet();
+
+$pet3->setName('Filbert');
+echo "Pet's name is " . $pet3->getName();
+
+
+$pet2->setColor('Cyan');
+echo "Pet's color is " . $pet2->getColor();
+
+echo "<pre>";
+var_dump($pet1);
+var_dump($pet2);
+var_dump($pet3);
+echo "</pre>";
 
 // Invoke the eat method
 $pet1->eat();
--- pet.php
+++ pet.php
@@ -9,9 +9,41 @@ class Pet
     private $_name;
     private $_color;
 
+    /**
+     * Set the pet's name
+     */
+    function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    /** Get the pet's name */
+    function getName()
+    {
+        return $this->_name;
+    }
+
+    function setColor($color)
+    {
+        $this->_color = $color;
+    }
+
+    /** Get the pet's name */
+    function getColor()
+    {
+        return $this->_name;
+    }
+
+    // Default constructor
+    function __construct($name="unknown", $color="???")
+    {
+        $this->_name = $name;
+        $this->_color = $color;
+    }
+
     function eat()
     {
-        echo "<p>" . $this->_name . " is eating</p>";
+        echo "<p>$this->_name is eating</p>";
     }
 
     function talk()
